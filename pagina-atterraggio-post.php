ada11481f0cb4084479ec66bc6e3a3aa9b8151a1
ValerioViteritti <vviteritti@gmail.com>
2024-07-23 18:03:10 +0200

    fix: minor

--- pagina-atterraggio-post.php
+++ pagina-atterraggio-post.php
@@ -9,7 +9,6 @@ $parola_da_censurare = $_POST['blind'];
 $text_diverso = str_replace($parola_da_censurare, '***', $text);
 $text_non_contare_asterischi = str_replace ('***', '', $text_diverso);
 $text_diverso_senza_spazi = str_replace(' ', '', $text_non_contare_asterischi);
-
 $lunghezza_text_diverso = strlen($text_diverso_senza_spazi);
 
 ?>
@@ -22,14 +21,10 @@ $lunghezza_text_diverso = strlen($text_diverso_senza_spazi);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina atterraggio POST</title>
 </head>
-<body>
-    
+<body>   
     <h2><mark>Testo 1:</mark> <?php echo $text?></h2>
     <strong>Questo testo ha <?php echo $lunghezza_text ?> caratteri</strong>
-
     <h2><mark>Testo diverso:</mark> <?php echo $text_diverso?></h2>
     <strong>Questo testo ha <?php echo $lunghezza_text_diverso ?> caratteri</strong>
-
-    
 </body>
 </html>
