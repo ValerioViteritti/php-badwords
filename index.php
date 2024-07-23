290b0556d5573b190a77996fa0eebec95d984043
ValerioViteritti <vviteritti@gmail.com>
2024-07-23 17:39:17 +0200

    fix: sistemato, esercizio completato

--- index.php
+++ index.php
@@ -3,17 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php-badwords</title>
+    <title>FORM POST</title>
 </head>
 <body>
-    <span>Premi per andare al Form Post</span>
-    <form action="form-post.php"> 
-        <button>--> Premi per Form Post <--</button>
-    </form>
-    <br>
-    <span>Premi per andare al Form Get</span>
-    <form action="form-get.php">
-        <button>--> Premi per Form Get <--</button>
+    <H1>FORM POST</H1>
+
+    <form action="pagina-atterraggio-post.php" method="POST">
+        <label for="text">Inserisci del testo e decidi quale parola censurare</label>
+        <br>
+        <hr>
+        <input type="text" placeholder="inserisci del testo" name="text">
+        <br>
+        <br>
+        <input type="text" placeholder="parola da censurare" name="blind">
+        <br>
+        <br>
+        <button type="submit">invio</button>
     </form>
 </body>
 </html>
